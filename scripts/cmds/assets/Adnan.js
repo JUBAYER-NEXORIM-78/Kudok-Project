927053d75e703a3bd7162c762ab01ef777f97d61
JUBAYER-NEXORIM-78 <jubavai827387@gmail.com>
2025-05-14 23:48:25 +0600

    Delete scripts/cmds directory

--- scripts/cmds/assets/Adnan.js
+++
@@ -1,8 +0,0 @@
-[
-  "https://i.imgur.com/MCfs1yj.mp4",
-  "https://i.imgur.com/MCfs1yj.mp4",
-  "https://i.imgur.com/MCfs1yj.mp4",
-  "https://i.imgur.com/MCfs1yj.mp4",
-  "https://i.imgur.com/MCfs1yj.mp4",
-  "https://i.imgur.com/MCfs1yj.mp4"
-]
